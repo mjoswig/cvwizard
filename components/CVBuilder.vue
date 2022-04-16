24084dc67126a5ecd0000b4e4803f6ddd728e266
Manuel Joswig <manuel@joswigsolutions.com>
2022-04-16 09:54:16 +0200

    Fixed responsiveness

--- components/CVBuilder.vue
+++ components/CVBuilder.vue
@@ -1,7 +1,7 @@
 <template>
-  <div style="width: 1340px;">
+  <div>
     <div class="flex justify-center mb-8 w-full">
-      <div class="bg-gray-700 font-bold text-center rounded-md flex">
+      <div class="bg-gray-700 font-bold text-center rounded-md grid grid-cols-2 md:grid-cols-4">
         <div class="p-4">
           <div class="flex justify-center">
             <div class="bg-blue-500 h-8 w-8 rounded-full mb-2"></div>
@@ -38,24 +38,26 @@
         </div>
       </div>
     </div>
-    <div class="bg-white p-12 shadow-lg w-full">
-      <div class="flex items-center">
-        <div class="w-1/4">
-          <div class="bg-gray-100 cursor-pointer h-64 w-64 flex items-center justify-center rounded-full">
-            <div>
-              <div class="flex justify-center mb-1">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-camera-fill" viewBox="0 0 16 16">
-                  <path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
-                  <path d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"/>
-                </svg>
+    <div class="overflow-scroll">
+      <div class="bg-white p-12 shadow-lg w-full" style="width: 1340px;">
+        <div class="flex items-center">
+          <div class="w-1/4">
+            <div class="bg-gray-100 cursor-pointer h-64 w-64 flex items-center justify-center rounded-full">
+              <div>
+                <div class="flex justify-center mb-1">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-camera-fill" viewBox="0 0 16 16">
+                    <path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                    <path d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"/>
+                  </svg>
+                </div>
+                <span class="font-bold text-gray-400 uppercase">Select your picture</span>
               </div>
-              <span class="font-bold text-gray-400 uppercase">Select your picture</span>
             </div>
           </div>
-        </div>
-        <div class="w-3/4">
-          <input class="block font-bold text-6xl w-full" type="text" placeholder="Your name" />
-          <input class="block font-bold text-2xl uppercase w-full" type="text" placeholder="Your profession or specialty" />
+          <div class="w-3/4">
+            <input class="block font-bold text-6xl w-full" type="text" placeholder="Your name" />
+            <input class="block font-bold text-2xl uppercase w-full" type="text" placeholder="Your profession or specialty" />
+          </div>
         </div>
       </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,7 @@
   <div class="page-content">
     <h1 class="text-purple-brand sm:text-center mb-4">Create a professional CV</h1>
     <p class="sm:text-center text-gray-400 text-lg mb-8">This page is still under construction. Please come back again later!</p>
-    <div class="overflow-scroll">
       <CVBuilder />
-    </div>
   </div>
 </template>
 
