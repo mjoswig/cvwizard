cf5c4438222f382ade86e6865eb4c8fe1a0bf976
Manuel Joswig <manuel@joswigsolutions.com>
2022-04-26 12:14:52 +0200

    Added google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-analytics'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
