eebb5d386542abe2a68c156306aca793913b9692
Manuel Joswig <manuel@joswigsolutions.com>
2022-04-28 22:35:04 +0200

    Updated software

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      //{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Save time and effort with cvwizard.io, the best professional resume maker & CV builder online. Impress recruiters with a modern PDF resume template and get the job you want.' },
       { hid: 'og:title', property: 'og:title', content: 'Professional CV Builder - cvwizard.io' },
       { hid: 'og:description', property: 'og:description', content: 'Save time and effort with cvwizard.io, the best professional resume maker & CV builder online. Impress recruiters with a modern PDF resume template and get the job you want.' },
