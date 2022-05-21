95819c2694c557856408c7d066ae6d73663e70ba
Manuel Joswig <manuel@joswigsolutions.com>
2022-05-21 12:46:35 +0200

    Added producthunt widget

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,7 +17,7 @@
       <main>
         <nuxt-child :is-downloading="isDownloading" />
       </main>
-      <footer class="flex flex-col items-center px-8 pb-8 pt-1 text-sm">
+      <footer class="flex flex-col items-center px-8 pb-8 pt-2 text-sm">
         <div class="mb-4">
           <a href="https://www.producthunt.com/posts/cvwizard-io?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-cvwizard&#0045;io" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=344605&theme=light" alt="cvwizard&#0046;io - Professional&#0032;CV&#0032;Builder | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
         </div>
