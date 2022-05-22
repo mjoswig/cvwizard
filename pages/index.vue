c91dd31e02cdf925ab3ddc5555d28bebe9542a8a
Manuel Joswig <manuel@joswigsolutions.com>
2022-05-22 22:02:23 +0200

    Added more jobs

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,9 @@
       <p class="lg:hidden text-gray-400 sm:text-center text-xl mt-4">You will get the best user experience by visiting this website on a large device.</p>
     </div>
     <CVBuilder :is-downloading="isDownloading" />
-    <div class="flex justify-center w-full">
-      <div class="w-full">
-        <adsbygoogle class="mt-12" />
-      </div>
+    <div class="w-full">
+      <adsbygoogle class="mt-12" />
+    </div>
     </div>
   </div>
 </template>
