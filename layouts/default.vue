15a15a91f86ddf4ee6e3eebfe922114c6360fd7e
Manuel Joswig <manuel@joswigsolutions.com>
2022-05-22 11:36:13 +0200

    Removed payment requirement

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,12 +6,12 @@
           <div>
             <nuxt-link to="/"><img class="w-24 h-auto" src="@/assets/images/logo.png" /></nuxt-link>
           </div>
-          <a class="cta-link-primary cursor-pointer flex items-center space-x-2" @click="showDownloadModal = true">
+          <Btn :is-loading="isDownloading" @click="processDownload">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-arrow-down-fill" viewBox="0 0 16 16">
               <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zm-1 4v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 11.293V7.5a.5.5 0 0 1 1 0z"/>
             </svg>
             <span>Download PDF</span>
-          </a>
+          </Btn>
         </div>
       </header>
       <main>
@@ -101,6 +101,14 @@ export default {
         }
       }
     },
+    processDownload() {
+      this.isDownloading = true
+      const self = this
+      setTimeout(() => {
+        self.download()
+        this.isDownloading = false
+      }, 500)
+    },
     async processPayment() {
       this.downloadModalLoading = true
       this.isDownloading = true
@@ -177,7 +185,7 @@ export default {
     }
   },
   mounted() {
-    this.initStripe()
+    //this.initStripe()
   }
 }
 </script>
