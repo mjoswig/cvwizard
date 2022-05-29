44d9efc0b3746036a1ee54eef5d300dc7a096ab2
Manuel Joswig <manuel@joswigsolutions.com>
2022-05-29 22:51:23 +0200

    Switched ad placeholder

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,16 @@
       <h1 class="text-purple-brand sm:text-center">Create a professional CV in 5 minutes</h1>
       <p class="lg:hidden text-gray-400 sm:text-center text-xl mt-4">You will get the best user experience by visiting this website on a large device.</p>
     </div>
-    <CVBuilder :is-downloading="isDownloading" />
-    <div class="w-full">
-      <adsbygoogle class="mt-12" />
-    </div>
+    <div class="flex">
+      <div class="w-1/4">
+        <adsbygoogle />
+      </div>
+      <div class="w-full">
+        <CVBuilder :is-downloading="isDownloading" />
+      </div>
+      <div class="w-1/4">
+        <adsbygoogle />
+      </div>
     </div>
   </div>
 </template>
