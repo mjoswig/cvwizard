98c8ee6fe1835bf02d8dbccf451ef1b530d6ee83
Manuel Joswig <manuel@joswigsolutions.com>
2023-08-16 17:21:02 +0200

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,12 +39,12 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    '@nuxtjs/google-fonts',
     '@nuxtjs/google-gtag',
     '@nuxtjs/sitemap'
   ],
