d032938c58cefc8e7cf425b8d11248dfc339aea8
Manuel Joswig <manuel@joswigsolutions.com>
2023-08-17 12:48:55 +0200

    Added SEO text

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,54 @@
 <template>
-  <div class="page-content">
-    <div class="mb-12">
-      <h1 class="text-purple-brand sm:text-center mb-4">Create a professional CV in 5 minutes</h1>
+  <article class="page-content">
+    <section class="mb-12">
+      <h1 class="text-purple-brand sm:text-center mb-4">The easiest way to create a CV</h1>
       <p class="sm:text-center text-xl">cvwizard is a free CV builder that offers a quick and easy way to create your CV online.<span class="lg:hidden"> You'll get the best user experience by visiting this website on a large device.</span></p>
-    </div>
-    <CVBuilder :is-downloading="isDownloading" />
-  </div>
+    </section>
+    <section class="mb-14">
+      <CVBuilder :is-downloading="isDownloading" />
+    </section>
+    <section class="mb-12">
+      <h2 class="sm:text-center mb-4">The best free CV builder in 2023</h2>
+      <p class="text-lg mb-8">Our CV Maker is designed to help you craft the perfect CV, no matter what stage of your career you're in. With a range of powerful features and a user-friendly interface, you'll be able to create a standout CV in just a few minutes.</p>
+      <div class="grid md:grid-cols-2 gap-8">
+        <div>
+          <h3 class="mb-2">No sign-up required</h3>
+          <p class="text-lg">Build and download your CV without having to create an account. All you need to do is enter your information and download your CV.</p>
+        </div>
+        <div>
+          <h3 class="mb-2">PDF download</h3>
+          <p class="text-lg">Our paid PDF CV download offers a high-quality, professionally designed CV that is perfect for printing or emailing to potential employers.</p>
+        </div>
+        <div>
+          <h3 class="mb-2">ATS-friendly</h3>
+          <p class="text-lg">Designed with applicant tracking systems (ATS) in mind, you can be confident that your CV will get seen by the right people.</p>
+        </div>
+        <div>
+          <h3 class="mb-2">Privacy-friendly</h3>
+          <p class="text-lg">We won't store any of your data. Save your CV data locally for complete control over your personal information.</p>
+        </div>
+      </div>
+    </section>
+    <section>
+      <h2 class="sm:text-center mb-8">Frequently asked questions</h2>
+      <div class="mb-8">
+        <h3 class="mb-2">How long should a CV be?</h3>
+        <p class="text-lg">A CV should be kept to a single page as it allows the employer to quickly review the applicant's qualifications and experience without getting overwhelmed by unnecessary details.</p>
+      </div>
+      <div class="mb-8">
+        <h3 class="mb-2">What should I put on my CV?</h3>
+        <p class="text-lg">When creating a CV, it's important to include only the relevant information for the role you're applying for, such as your education, work experience, skills, and achievements.</p>
+      </div>
+      <div class="mb-8">
+        <h3 class="mb-2">How to list references on a CV?</h3>
+        <p class="text-lg">It's generally recommended not to list references on a CV. Instead, you can include a separate document with your references, which can be provided upon request from the employer.</p>
+      </div>
+      <div>
+        <h3 class="mb-2">How should a CV look?</h3>
+        <p class="text-lg">When it comes to the appearance of a CV, simplicity and readability are key, and it's generally best to avoid overly creative design elements that could be distracting or hard to read.</p>
+      </div>
+    </section>
+  </article>
 </template>
 
 <script>
@@ -14,3 +57,13 @@ export default {
   props: ['isDownloading']
 }
 </script>
+
+<style scoped>
+.page-content {
+  @apply flex flex-col items-center justify-center w-full;
+}
+.page-content section {
+  @apply w-full;
+  max-width: 21cm !important;
+}
+</style>
