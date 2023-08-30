54039951486df4e9ead5fb3346b4754158638970
Manuel Joswig <manuel@joswigsolutions.com>
2023-08-30 16:20:09 +0200

    Dynamic generation of categories

--- pages/cv-templates/index.vue
+++ pages/cv-templates/index.vue
@@ -1,26 +1,18 @@
 <template>
   <article class="page-content">
-    <section class="mb-12">
+    <section>
       <h1 class="text-purple-brand sm:text-center mb-4">Free CV templates</h1>
       <p class="sm:text-center text-xl mb-6">Our CV templates will show you the skills and achievements you need to land work in {{ currentYear }}.</p>
       <div class="flex sm:justify-center text-lg w-full">
         <Btn @click="$router.push('/')">Create your CV - it's free</Btn>
       </div>
     </section>
-    <section>
-      <h2 class="mb-7">Programming CV examples</h2>
+    <section class="mt-12" v-for="(category, index) in cvCategories" :key="index">
+      <h2 class="mb-7">{{ category.name }} CV examples</h2>
       <div class="grid grid-cols-2 md:grid-cols-3 gap-3 md:gap-6">
-        <nuxt-link to="/cv-templates/salesforce-developer-operations">
-          <img class="shadow-sm mb-3 md:mb-6" src="@/assets/images/cv-data/salesforce-developer-operations.png" />
-          <span class="block text-lg md:text-xl">Salesforce Developer - Operations CV</span>
-        </nuxt-link>
-        <nuxt-link to="/cv-templates/artificial-intelligence-researcher">
-          <img class="shadow-sm mb-3 md:mb-6" src="@/assets/images/cv-data/artificial-intelligence-researcher.png" />
-          <span class="block text-lg md:text-xl">Artificial Intelligence Researcher CV</span>
-        </nuxt-link>
-        <nuxt-link to="/cv-templates/senior-quality-engineer">
-          <img class="shadow-sm mb-3 md:mb-6" src="@/assets/images/cv-data/senior-quality-engineer.png" />
-          <span class="block text-lg md:text-xl">Senior Quality Engineer CV</span>
+        <nuxt-link v-for="(cvSlug, index) in category.cvs" :key="index" :to="`/cv-templates/${cvSlug}`">
+          <img class="shadow-sm mb-3 md:mb-6" :src="require(`@/assets/images/cv-data/${cvSlug}.png`)" />
+          <span class="block text-lg md:text-xl">{{ require(`@/assets/json/cv-data/${cvSlug}.json`).profession.value }}</span>
         </nuxt-link>
       </div>
     </section>
@@ -28,11 +20,18 @@
 </template>
 
 <script>
+import cvCategories from '@/assets/json/cv-categories.json'
+
 export default {
   name: 'CvTemplatesIndexPage',
   head() {
     return {
       title: 'Free CV Templates - cvwizard'
+    }
+  },
+  data() {
+    return {
+      cvCategories
     }
   },
   computed: {
